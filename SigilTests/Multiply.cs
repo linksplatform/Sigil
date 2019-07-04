33c0fe5f94e5c8e4d88742cfb85f5b4db5ed5b17
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2019-07-04 17:30:20 +0200

    Ingnoring the mul.ovf tests for .NET Core due to differences in behaviour with non-integer parameters

--- SigilTests/Multiply.cs
+++ SigilTests/Multiply.cs
@@ -22,35 +22,53 @@ namespace SigilTests
 
             var d1 = e1.CreateDelegate();
 
-            Assert.AreEqual(3.14 * 1.59, d1(3.14, 1.59));
+            Assert.AreEqual(3.14 * 1.59, d1(3.14, 1.59), 0.000001);
         }
 
         [Test]
         public void Overflow()
         {
-            var e1 = Emit<Func<double, double, double>>.NewDynamicMethod("E1");
+            var e1 = Emit<Func<int, int, int>>.NewDynamicMethod("E1");
             e1.LoadArgument(0);
             e1.LoadArgument(1);
             e1.MultiplyOverflow();
             e1.Return();
 
             var d1 = e1.CreateDelegate();
+            Assert.AreEqual(3 * 5, d1(3, 5));
 
-            Assert.AreEqual(3.14 * 1.59, d1(3.14, 1.59));
+            var e2 = Emit<Func<double, double, double>>.NewDynamicMethod("E2");
+            e2.LoadArgument(0);
+            e2.LoadArgument(1);
+            e2.MultiplyOverflow();
+            e2.Return();
+
+            var d2 = e2.CreateDelegate();
+            Assert.AreEqual(3.14 * 1.59, d2(3.14, 1.59), 0.000001);
         }
 
         [Test]
         public void UnsignedOverflow()
         {
-            var e1 = Emit<Func<double, double, double>>.NewDynamicMethod("E1");
+            var e1 = Emit<Func<int, int, int>>.NewDynamicMethod("E1");
             e1.LoadArgument(0);
             e1.LoadArgument(1);
             e1.UnsignedMultiplyOverflow();
             e1.Return();
 
             var d1 = e1.CreateDelegate();
+            Assert.AreEqual(3 * 5, d1(3, 5));
 
-            Assert.AreEqual(3.14 * 1.59, d1(3.14, 1.59));
+#if !NETCOREAPP
+            var e2 = Emit<Func<double, double, double>>.NewDynamicMethod("E2");
+            e2.LoadArgument(0);
+            e2.LoadArgument(1);
+            e2.UnsignedMultiplyOverflow();
+            e2.Return();
+
+            var d2 = e2.CreateDelegate();
+            Assert.AreEqual(3.14 * 1.59, d2(3.14, 1.59), 0.000001);
+#endif
         }
     }
 }
